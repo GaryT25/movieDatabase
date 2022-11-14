d0c60da64627a53c5fd784e047721e522e6c052c
GaryT25 <truegarythompson@gmail.com>
2022-11-14 15:58:57 -0500

    Search Button Now works!

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const API_KEY = 'api_key=d21fe53da0959aa24c92f7e7796201a4';
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const API_URL = BASE_URL + 'discover/movie?sort_by=popularity.desc&'+API_KEY;
-
-getMovies(API_URL);
-const IMG_URL ='https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg';
-
-
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL ='https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
-const main = document.getElementById(`main`)
-const form = document.getElementById(`form`);
-const search = document.getElementById(`search`);
+getMovies(API_URL);
+
+
+
+const main = document.getElementById('main')
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
 
 
@@ -24,14 +24,15 @@ function getMovies(url) {
 
 
 function showMovies(data) {
-    main.innerHTML ='';
+    main.innerHTML = '';
 
 
     data.forEach(movie => {
-        const{title, poster_path, vote_average, overview} = movie;
+        const{title,poster_path, vote_average, overview} = movie;
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
-        movieEl.innerHTML = `<img src="${IMG_URL+poster_path}" alt="${title}">
+        movieEl.innerHTML = `
+        <img src= "${IMG_URL+poster_path}" alt="${title}">
         <div class="movie-info">
             <h3>${title}</h3>
             <span class="${getColor(vote_average)}">${vote_average}</span>
@@ -70,6 +71,6 @@ form.addEventListener('submit',(e) => {
     const searchTerm = search.value;
 
     if(searchTerm) {
-        getMovies(searchURL+'$query='+searchTerm);
+        getMovies(searchURL+'&query='+searchTerm);
     }
 })

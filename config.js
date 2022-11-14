41acb5cebffd328e8d38d86fa757c78b03f164d6
GaryT25 <truegarythompson@gmail.com>
2022-11-14 15:27:18 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,75 @@
+const API_KEY = 'api_key=d21fe53da0959aa24c92f7e7796201a4';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_URL = BASE_URL + 'discover/movie?sort_by=popularity.desc&'+API_KEY;
+
+getMovies(API_URL);
+const IMG_URL ='https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg';
+
+
+const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+
+const main = document.getElementById(`main`)
+const form = document.getElementById(`form`);
+const search = document.getElementById(`search`);
+
+
+
+function getMovies(url) {
+
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results);
+        showMovies(data.results);
+    })
+}
+
+
+function showMovies(data) {
+    main.innerHTML ='';
+
+
+    data.forEach(movie => {
+        const{title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `<img src="${IMG_URL+poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+        </div>
+        
+        
+        
+        
+        `
+        // this below is important
+        main.appendChild(movieEl);
+    });
+}
+
+
+
+function getColor(vote){
+    if(vote >= 8){
+        return 'green'
+    }
+    else if(vote>=5){
+        return 'orange'
+    }
+    else{
+        return 'red'
+    }
+}
+
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm) {
+        getMovies(searchURL+'$query='+searchTerm);
+    }
+})
